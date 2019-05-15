b07a79a7d0ae98232c55856a87006493020d2ed6
LoadingLoading <tzy.con@qq.com>
2019-05-15 18:26:10 +0100

    new one

--- HuffmanOriginal.py
+++ HuffmanOriginal.py
@@ -4,7 +4,7 @@ import queue
 
 class HuffmanNode(object):
     def __init__(self, left=None, right=None, root=None):
-        self.left = lefta
+        self.left = leftaa
         self.right = right
     def children(self):
         return((self.left, self.right))
